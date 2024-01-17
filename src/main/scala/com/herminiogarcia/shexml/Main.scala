2ab5e0b78d7ad648e2c49089b2ab4f2da9d02c4c
Herminio García González <herminiogg@hotmail.com>
2024-01-17 10:37:36 +0100

    Version 0.4.2

--- src/main/scala/com/herminiogarcia/shexml/Main.scala
+++ src/main/scala/com/herminiogarcia/shexml/Main.scala
@@ -17,7 +17,7 @@ object Main {
 
 }
 
-@Command(name = "ShExML", version = Array("v0.4.1"),
+@Command(name = "ShExML", version = Array("v0.4.2"),
   mixinStandardHelpOptions = true,
   description = Array("Map and merge heterogeneous data sources with a Shape Expressions based syntax"))
 class Main extends Callable[Int] {
