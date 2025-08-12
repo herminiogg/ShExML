7f0faf952d009d4be4b1f2a3e13fcbd887dd2282
Herminio García González <herminiogg@hotmail.com>
2025-08-12 15:23:10 +0200

    Refactoring of the cross-compiled classes to avoid the duplication of code as much as possible.

--- src/main/scala-2.12/com/herminiogarcia/shexml/helper/ParallelExecutionConfigurator.scala
+++ src/main/scala-2.12/com/herminiogarcia/shexml/helper/ParallelExecutionConfigurator.scala
@@ -1,35 +1,13 @@
 package com.herminiogarcia.shexml.helper
 
-import scala.collection.parallel.{ForkJoinTaskSupport, ParSeq}
-import scala.util.Try
+import scala.collection.parallel.ParSeq
 
-class ParallelExecutionConfigurator(val parallelisationAspectsOptions: Map[String, Boolean], val numberOfThreads: Option[Int]) {
+class ParallelExecutionConfigurator(override val parallelisationAspectsOptions: Map[String, Boolean], override val numberOfThreads: Option[Int])
+  extends ParallelExecutionConfiguratorGen(parallelisationAspectsOptions, numberOfThreads) {
 
-  def asParallelCollection[T](aspect: String, collection: Seq[T]): Either[Seq[T], ParSeq[T]] = {
-    if(!parallelisationAspectsOptions.getOrElse(aspect, false)) Left(collection)
-    else numberOfThreads match {
-      case Some(number) =>
-        collection.par
-        val parallelCollection = collection.par
-        parallelCollection.tasksupport = new ForkJoinTaskSupport(new java.util.concurrent.ForkJoinPool(number))
-        Right(parallelCollection)
-      case None => Right(collection.par)
-    }
+  override protected def convertToParallelCollection[T](collection: Seq[T]): ParSeq[T] = {
+    collection.par
   }
 }
 
-object ParallelExecutionConfigurator {
-
-  def apply(parallelAspects: String, numberOfThreads: String): ParallelExecutionConfigurator = {
-    val parallelAspectsOptions =
-      if(parallelAspects.toLowerCase == "queries") Map[String, Boolean]("queries" -> true)
-      else if(parallelAspects.toLowerCase == "shapes") Map[String, Boolean]("shapes" -> true)
-      else Map[String, Boolean]("shapes" -> true, "queries" -> true)
-    new ParallelExecutionConfigurator(
-      parallelAspectsOptions,
-      Try(numberOfThreads.toInt).filter(_ > 0).toOption
-    )
-  }
-
-  def empty = new ParallelExecutionConfigurator(Map(), None)
-}
+object ParallelExecutionConfigurator extends ParallelExecutionConfiguratorTrait
